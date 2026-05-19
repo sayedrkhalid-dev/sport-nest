ea1d1c65dc0d2b8bce76a3f75a78ad5c8aa07446
Sayed <sayedrkhalid.official@gmail.com>
2026-05-19 16:06:36 +0600

    chore: configure Prettier formatting and eslint flat config integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  eslintConfigPrettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
