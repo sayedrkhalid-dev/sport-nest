e7e87d9e342ddb77a26ab875f5d0a9d427ecc0c9
Sayed <sayedrkhalid.official@gmail.com>
2026-06-08 01:53:14 +0600

    fixed app url in authClint

--- src/lib/authClient.js
+++ src/lib/authClient.js
@@ -3,7 +3,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
