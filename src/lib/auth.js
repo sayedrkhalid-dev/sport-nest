25d1872285b2c1555f3e7c65438a3efa630af97b
Sayed <sayedrkhalid.official@gmail.com>
2026-06-07 23:06:27 +0600

    fixed public url

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,7 +6,7 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db();
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
   secret: process.env.BETTER_AUTH_SECRET,
   database: mongodbAdapter(db),
   emailAndPassword: { enabled: true },
