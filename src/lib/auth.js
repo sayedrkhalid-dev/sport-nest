571982d68f9758ef1eea9a27787b1c270d3dc5be
Sayed <sayedrkhalid.official@gmail.com>
2026-06-08 00:02:31 +0600

    fixed better auth import mistake

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,3 +1,10 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db();
+
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   secret: process.env.BETTER_AUTH_SECRET,
